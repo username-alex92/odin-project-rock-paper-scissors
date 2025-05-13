9158e5c5f25e83dd7064cc5fa5f38ea8e523cf31
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-14 01:02:57 +0300

    Refactor code, change text, add ui
    In the new version, the player can play the game by pressing buttons instead of typing their answer in a prompt. Round and game results are now displayed on the screen instead of in the console.

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let humanScore = 0;
+let computerScore = 0;
+let result;
+
+const rockSelection = document.createElement("button");
+rockSelection.textContent = "Rock";
+const paperSelection = document.createElement("button");
+paperSelection.textContent = "Paper";
+const scissorsSelection = document.createElement("button");
+scissorsSelection.textContent = "Scissors";
+
+const buttons = document.createElement("div");
+const roundResult = document.createElement("div");
+
+let runningScore = document.createElement("div");
+runningScore.textContent = `Player ${humanScore} : ${computerScore} Computer`;
+
+document.body.appendChild(buttons);
+buttons.appendChild(rockSelection);
+buttons.appendChild(paperSelection);
+buttons.appendChild(scissorsSelection);
+document.body.appendChild(roundResult);
+document.body.appendChild(runningScore);
+
+buttons.addEventListener("click", (event) => {
+    let choice = event.target.textContent;
+    result = playRound(choice, getComputerChoice());
+
+    if (result === "human") {
+        humanScore++;
+    } else if (result === "computer") {
+        computerScore++;
+    }
+
+    runningScore.textContent = `Player ${humanScore} : ${computerScore} Computer`;
+
+    if (humanScore === 5 || computerScore === 5) {
+        resetDisplay();
+    }
+});
+
 // The function randomly returns “rock”, “paper” or “scissors”.
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -11,13 +52,6 @@ function getComputerChoice() {
     }
 }
 
-
-// The function takes the user choice and returns it.
-function getHumanChoice() {
-    return prompt("Let's play Rock, Paper, Scissors! Type your choice: rock, paper, or scissors.");
-}
-
-
 // The function that takes the human and computer player choices as arguments, 
 // plays a single round, increments the round winner’s score and logs a winner announcement.
 function playRound(humanChoice, computerChoice) {
@@ -26,94 +60,27 @@ function playRound(humanChoice, computerChoice) {
     if ((humanChoice === "rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        roundResult.textContent = `You won, ${humanChoice} beats ${computerChoice}`;
         return "human";
     } else if (humanChoice === computerChoice) {
-        console.log(`Draw! Your choice ${humanChoice}, computer choice ${computerChoice}`);
-        return "";
+        roundResult.textContent = `Draw, your choice ${humanChoice}, computer choice ${computerChoice}`;
+        return "draw";
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        roundResult.textContent = `You lose, ${computerChoice} beats ${humanChoice}.`;
         return "computer";
     }
 }
 
+function resetDisplay() {
+    displayFinalResult(humanScore, computerScore);
+    humanScore = 0;
+    computerScore = 0;
+}
 
-// Human selection
-const rockSelection = document.createElement("button");
-rockSelection.textContent = "Rock";
-const paperSelection = document.createElement("button");
-paperSelection.textContent = "Paper";
-const scissorsSelection = document.createElement("button");
-scissorsSelection.textContent = "Scissors";
-
-
-let runningScore = document.createElement("div");
-runningScore.textContent = `Player ${humanScore} : ${computerScore} Computer`;
-
-document.body.appendChild(rockSelection);
-document.body.appendChild(paperSelection);
-document.body.appendChild(scissorsSelection);
-document.body.appendChild(runningScore);
-
-rockSelection.addEventListener("click", () => {
-    playRound(rockSelection.textContent, getComputerChoice());
-});
-
-paperSelection.addEventListener("click", () => {
-    playRound(paperSelection.textContent, getComputerChoice());
-});
-
-scissorsSelection.addEventListener("click", () => {
-    playRound(scissorsSelection.textContent, getComputerChoice());
-});
-
-
-
-
-
-
-// function displayFinalResult(humanScore, computerScore) {
-//     console.log("Game over!");
-//     if (humanScore > computerScore) {
-//         console.log(`You won! ${humanScore} : ${computerScore}`);
-//     } else if (computerScore > humanScore) {
-//         console.log(`You lose! ${computerScore} : ${humanScore}`);
-//     } else {
-//         console.log(`Draw! ${humanScore} : ${computerScore}`);
-//     }
-// }
-
-
-// The function calls playRound() to play 5 rounds, keeps track of the scores and declares 
-// a winner at the end.
-// function playGame() {
-//     // Player score variables.
-//     let humanScore = 0;
-//     let computerScore = 0;
-
-//     for (let i = 0; i < 5; i++) {
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
-//         let result = playRound(humanSelection, computerSelection);
-
-//         if (result === "human") {
-//             humanScore++;
-//         } else if (result === "computer") {
-//             computerScore++;
-//         }
-
-//         console.log(`Match score: ${humanScore} : ${computerScore}`);
-//     }
-
-//     displayFinalResult(humanScore, computerScore);
-// }
-
-// playGame();
-
-// if (result === "human") {
-//     humanScore++;
-//     runningScore.textContent = `Player ${humanScore} : ${computerScore} Computer`;
-// } else if (result === "computer") {
-//     computerScore++;
-//     runningScore.textContent = `Player ${humanScore} : ${computerScore} Computer`;
-// }
+function displayFinalResult(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+        runningScore.textContent = `Game over! You won! ${humanScore} : ${computerScore}`;
+    } else if (computerScore > humanScore) {
+        runningScore.textContent = `Game over! You lose! ${humanScore} : ${computerScore}`;
+    }
+}
